da6cd453ff5b8d69f8eedc195a7d921e5f20bf98
Ademez <ademolaoladosu1@gmail.com>
2024-02-20 17:56:14 +0100

    Fix: Answers to the JavaScript  Questions

--- assign.js
+++ assign.js
@@ -1,1 +1,56 @@
+var result = true;
+
+
+let userName = "Ademola"
+
+let favoriteNumber = 100
+
+let greeting;
+greeting = "Hello, World!"
+
+const pi = 3.14
+
+let age = 60
+typeof(age)
+
+let typeOfAge = "number"
+
+let num1, num2;
+num1 = 10;
+num2 = 20;
+
+let sum = num1 + num2;
+
+let isRaining = false
+let course = "JavaScript Basics"
+let count = 5
+count = 10
+
+let isJavaScriptFun = true
+const city = "Lagos"
+
+let myArray = [10, "Market", true]
+
+let temperature 
+temperature = 25
+
+const price = 20
+price  = price * 2
+
+console.log("Hello, JavaScript!")
+
+let firstName, lastName;
+firstName = "Ademola"
+lastName = "Oladosu"
+let fullName = firstName + lastName
+console.log(fullName)
+
+let isStudent = true
+let carBrand = "Toyota"
+carBrand = Honda
+
+let counter = 1
+counter = counter + 3
+
+
 
